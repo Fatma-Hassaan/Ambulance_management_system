9cd23a367468f919132264805222524f1910915d
RaedFrase <167563020+RaedFrase@users.noreply.github.com>
2024-11-22 22:52:03 +0200

    Add files via upload

---
+++ Hospital.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "LinkedQueue.h"
+#include "priQueue.h"
+#include "Patient.h"
+#include "Car.h"
+
+class Hospital
+{
+private:
+	int TS=0;					//timeStep
+	int H_ID;
+	int total_SC;
+	int total_NC;
+	int free_SC;
+	int free_NC;
+	int NC_Speed;
+	int SC_Speed;
+	int Current_NP_number;
+	int Current_SP_number;
+	int Current_EP_number;
+	LinkedQueue<Patient*> NP;
+	LinkedQueue<Patient*> SP;
+	priQueue<Patient*> EP;
+	LinkedQueue<Car*> NC;
+	LinkedQueue<Car*> SC;
+public:
+	Hospital(int Hospital_ID, int t_NC, int t_SC, int NC_S, int SC_S);
+	void incrementTimeStep();
+	int getHospital_ID();
+	int getNumber_FreeNC();
+	int getNumber_FreeSC();
+	int getNumber_CurrentEP();
+	void RecievePatient(Patient* P);
+	Car* AssigningPatient();
+};
---
+++ Organizer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "LinkedQueue.h"
+#include "priQueue.h"
+#include "Patient.h"
+#include "Car.h"
+#include "Hospital.h"
+#include <string>
+using namespace std;
+
+class Organizer
+{
+private:
+    int timeStep=0;
+	int numOfHospitals;
+	Hospital** HospitalsList;
+	int** DistanceMatrix;
+    int NCS;                    //Normal Car Speed
+    int SCS;                    //Special Car Speed
+    int numOfAR;                //number of All Requests
+	int numOfReDisEP;			//number of Redistributed Emergency Patients
+    int numOfCR;                //number of Cancelled Requests
+	int numOfOC;				//number of Out Cars
+	int numOfBC;				//number of Back Cars
+	LinkedQueue<Patient*> AR;	//All Requests
+	LinkedQueue<Patient*> FP;	//Finished Patients
+	LinkedQueue<Patient*> CR;	//Cancelled Requests
+	priQueue<Car*> OC;			//Out Cars
+	priQueue<Car*> BC;			//Back Cars
+public:
+	Organizer(string fileName);
+    void incrementTimeStep_and_Execute();
+    void AddingPatients();
+    void HospitalsAssigningPatients();
+    ~Organizer();
+};
